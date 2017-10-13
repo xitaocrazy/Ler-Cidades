71f406e580bf1661fea5b840890b5a666a55d838
Daniel "Xitão" Martins <xitaocrazy@gmail.com>
2017-10-12 23:52:45 -0300

    Correção no encoding
    Correção no encoding

--- src/services/LeitorDeCsvService.java
+++ src/services/LeitorDeCsvService.java
@@ -1,9 +1,11 @@
 package services;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +20,7 @@ public class LeitorDeCsvService implements ILeitorDeCidades {
 	    String linha = "";
 	    String csvDivisor = ",";
 	    try {
-	        buffer = new BufferedReader(new FileReader(caminho));
+	        buffer = new BufferedReader(new InputStreamReader(new FileInputStream(caminho), "UTF-8"));
 	        linha = buffer.readLine();
 	        while ((linha = buffer.readLine()) != null) {	        	
 	            String[] cidade = linha.split(csvDivisor);
